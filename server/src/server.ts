2525fec5f7ebcf79ab67839136cf35728f3d51d2
Quinn Brown <184797872+Beazzty@users.noreply.github.com>
2025-04-28 22:07:00 -0400

    server runs

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,42 +1,49 @@
 import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
-import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
-import http from 'http';
 import cors from 'cors';
-import bodyParser from 'body-parser';
 
-import db from './config/connection';
-import { authMiddleware } from './utils/auth';
-import typeDefs from './schemas/typeDefs';
-import resolvers from './schemas/resolvers';
+import db from './config/connection.js';
+import { authMiddleware } from './utils/auth.js';
+import typeDefs from './schemas/typeDefs.js';
+import resolvers from './schemas/resolvers.js';
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const httpServer = http.createServer(app);
 
 async function startApolloServer() {
+
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
 
   await server.start();
 
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(cors<cors.CorsRequest>());
+
   app.use(
     '/graphql',
-    cors<cors.CorsRequest>(),
-    bodyParser.json(),
     expressMiddleware(server, {
-      context: authMiddleware,
+      context: authMiddleware as any,
     })
   );
 
-  db.once('open', () => {
-    httpServer.listen(PORT, () => {
-      console.log(`🌍 Server ready at http://localhost:${PORT}/graphql`);
+  // if we're in production, serve client/dist as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('../client/dist'));
+
+    app.get('*', (_req, res) => {
+      res.sendFile('../client/dist/index.html');
     });
+  }
+
+  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+  app.listen(PORT, () => {
+    console.log(`🌍 Server ready at http://localhost:${PORT}/graphql`);
   });
 }
 
