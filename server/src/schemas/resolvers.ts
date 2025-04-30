9229c5a1f8dc5eb85e20f67d48960bb3ef7ef502
iaplesnyc <iaplesnyc@gmail.com>
2025-04-30 19:34:49 -0400

    Fix server issues, update imports, and resolve TypeScript errors

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -1,44 +1,34 @@
-import { User } from '../models';
-import { signToken } from '../utils/auth';
+import User from '../models/User.js'; // Add .js extension
 import { AuthenticationError } from 'apollo-server-express';
+import { signToken } from '../utils/auth.js'; // Add .js extension
+import { IUser } from '../models/User';
 
 const resolvers = {
   Query: {
-    me: async (_parent, _args, context) => {
+    me: async (_parent: unknown, _args: unknown, context: { user: IUser }) => {
       if (context.user) {
         return await User.findById(context.user._id);
       }
       throw new AuthenticationError('You must be logged in');
     },
   },
-
   Mutation: {
-    addUser: async (_parent, { input }) => {
+    addUser: async (_parent: unknown, { input }: { input: IUser }) => {
       const user = await User.create(input);
-      const token = signToken(user);
+      const token = signToken(user.toObject()); // Convert Mongoose document to plain object
       return { token, user };
     },
-
-    login: async (_parent, { input }) => {
+    login: async (_parent: unknown, { input }: { input: { email: string; password: string } }) => {
       const user = await User.findOne({ email: input.email });
-
       if (!user) {
         throw new AuthenticationError('No user found with this email');
       }
-
       const isPwCorrect = await user.isCorrectPassword(input.password);
-
       if (!isPwCorrect) {
         throw new AuthenticationError('Incorrect credentials');
       }
-
-      const token = signToken(user);
+      const token = signToken(user.toObject()); // Convert Mongoose document to plain object
       return { token, user };
-    },
-
-    // Placeholder for future stat saving
-    saveStat: async () => {
-      throw new Error('saveStat not implemented yet');
     },
   },
 };
--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -1,73 +1,30 @@
 import { gql } from 'apollo-server-express';
 
 const typeDefs = gql`
-  enum Level {
-    Beginner
-    Intermediate
-    Advanced
-  }
-
-  type Vocab {
-    _id: ID!
-    word: String!
-    translation: String!
-    level: Level!
-  }
-
-  type VocabProgress {
-    vocabId: ID!
-    correct: Int
-    incorrect: Int
-  }
-
   type User {
-    _id: ID!
-    username: String!
-    email: String!
-    skillLevel: Level!
-    vocabProgress: [VocabProgress!]!
+    _id: ID
+    username: String
+    email: String
   }
 
-  type Stat {
-    _id: ID!
-    user: User!
-    vocab: Vocab!
-    correct: Boolean!
-    createdAt: String!
+  type Auth {
+    token: String
+    user: User
   }
 
-  input AddUserInput {
-    username: String!
-    email: String!
-    password: String!
-    skillLevel: Level!
-  }
-
-  input LoginInput {
-    email: String!
-    password: String!
-  }
-
-  input SaveStatInput {
-    vocabId: ID!
-    correct: Boolean!
-  }
-
-  type AuthPayload {
-    token: String!
-    user: User!
+  input UserInput {
+    username: String
+    email: String
+    password: String
   }
 
   type Query {
     me: User
-    flashcardsByLevel(level: Level!): [Vocab!]!
-    statsByUser(userId: ID!): [Stat!]!
   }
 
   type Mutation {
-    addUser(input: AddUserInput!): AuthPayload!
-    login(input: LoginInput!): AuthPayload!
-    saveStat(input: SaveStatInput!): Stat!
+    addUser(input: UserInput): Auth
+    login(input: UserInput): Auth
   }
 `;
 
