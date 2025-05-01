483e7f8a2c10dd35d3ba65ab9f572224bf146ed3
Codergeek081 <issefarah15@gmail.com>
2025-05-01 19:08:02 -0400

    Query And Mutations
    Added Query and Mutations

--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -40,3 +40,25 @@ userId: ID!
 numQuestions: int!
 skillLevel: SkillLevel!
 }
+
+type Query {
+    # Return the current logged-in user
+    me: User
+
+    # Fetch a quiz (list of flashcards) for the requested level
+    quiz(skillLevel: SkillLevel!): [Vocab!]!
+  }
+
+  type Mutation {
+    # Create a new user account
+    addUser(input: UserInput!): User!
+
+    # Record a quiz result (updates User.vocabProgress under the hood)
+    addResult(input: ResultInput!): User!
+
+    # Remove one past Result by its ID
+    removeResult(resultId: ID!): User!
+  }
+`;
+
+export default typeDefs;
