1c9dce0c1c655fea7fd5840a1119abc6594e7d58
Codergeek081 <issefarah15@gmail.com>
2025-05-05 14:38:09 -0400

    Updated seeder
    1. Updated JSON files for type validation.
    2. Added logic to transform password by hashing before seeding to DB.
    3. Added `skillLevel` as a token. Made sure to capitalize it in case if JSON file have it lower case.

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -1,15 +1,26 @@
 import db from "../config/connection.js";
-import {User} from "../models/index.js";
+import { User, VocabSet } from "../models/index.js";
 import cleanDB from "./cleanDB.js";
 
-
+import bcrypt from 'bcrypt';
 import UserData from './UserData.json' with { type: "json" };
+import VocabData from './VocabData.json' with { type: "json" };
 
 db.once('open', async () => {
   await cleanDB();
 
-  await User.insertMany(UserData);
+  const normalizedUsers = await Promise.all(
+    UserData.map(async user => ({
+      ...user,
+      password: await bcrypt.hash(user.password, 10),
+      skillLevel: user.skillLevel.toUpperCase(), // This line normalizes the case
+    }))
+  );
+
+  await User.insertMany(normalizedUsers);
+  await VocabSet.insertMany(VocabData);
 
   console.log('User seeded!');
+  console.log('Vocab seeded!');
   process.exit(0);
 });
