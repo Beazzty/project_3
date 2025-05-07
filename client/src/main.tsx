c033638f1a4f5afc2ca7700059a27cf1d87cc362
Codergeek081 <issefarah15@gmail.com>
2025-05-07 09:04:00 -0400

    Authentication
    Fixed Authentication

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,35 +1,58 @@
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-import App         from './App';
-import Welcome     from './pages/Welcome';
-import SignUp      from './pages/SignUp';
-import Login       from './pages/Login';
-import SkillSelect from './pages/SkillSelect';
-import Beginner    from './pages/Beginner';
-import Intermediate from './pages/Intermediate';
-import Advanced    from './pages/Advanced';
-import Stats       from './pages/Stats';
-import NotFound    from './pages/NotFound';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+
+import App from './App'
+import WelcomePage from './components/pages/WelcomePage'
+import LoginPage from './components/pages/LoginPage'
+import RegisterPage from './components/pages/RegisterPage'
+import QuizPage from './components/pages/QuizPage'
+import StatsPage from './components/pages/StatsPage'
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+  credentials: 'same-origin',
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+})
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <NotFound/>,
     children: [
-      { index: true,          element: <Welcome/>    },
-      { path: 'signup',       element: <SignUp/>     },
-      { path: 'login',        element: <Login/>      },
-      { path: 'select',       element: <SkillSelect/>},
-      { path: 'beginner',     element: <Beginner/>   },
-      { path: 'intermediate', element: <Intermediate/> },
-      { path: 'advanced',     element: <Advanced/>   },
-      { path: 'stats',        element: <Stats/>      },
-    ]
-  }
-]);
+      { index: true, element: <WelcomePage /> },
+      { path: 'login', element: <LoginPage /> },
+      { path: 'register', element: <RegisterPage /> },
+      { path: 'quiz', element: <QuizPage /> },
+      { path: 'stats', element: <StatsPage /> },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router}/>
-);
+  <ApolloProvider client={client}>
+    <RouterProvider router={router} />
+  </ApolloProvider>
+)
